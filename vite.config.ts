1bebf6d66946bfb6fd4f46f8fc1d3cd7bfcc9eef
Artem Khrustalev <khru-artem@yandex.ru>
2025-01-11 20:02:18 +0300

    test deploy_3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://khruartem.github.io/'
+  base: 'https://khruartem.github.io/otkter'
 })
