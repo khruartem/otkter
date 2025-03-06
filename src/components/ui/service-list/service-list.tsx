28bcb185c9dc6595d3470d5e35016173c23b1913
Artem Khrustalev <khru-artem@yandex.ru>
2025-03-06 22:17:58 +0300

    fix:баг центрирования последней карточки на мобилке

--- src/components/ui/service-list/service-list.tsx
+++ src/components/ui/service-list/service-list.tsx
@@ -4,14 +4,20 @@ import { TServiceListUIProps } from "./types";
 
 import styles from "./service-list.module.css";
 import { Service } from "../../service";
+import { useGetMediaQuery } from "../../../hooks/useGetMediaQuery";
+import clsx from "clsx";
 
 export const ServiceListUI: FC<TServiceListUIProps> = ({
   services,
   servicesRefs,
   servicesViewRefs,
 }) => {
+  const { isMobile } = useGetMediaQuery();
   return (
-    <div className={styles["service-list"]}>
+    <div className={clsx(
+      styles["service-list"],
+      isMobile && styles["service-list_mobile"]
+    )}>
       {services.map((service) => {
         return (
           <Service
