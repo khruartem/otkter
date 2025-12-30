43e23ae1721a837db4d259ccee2d93e31025acc3
Artem Khrustalev <khru-artem@yandex.ru>
2025-12-31 01:44:15 +0300

    Updates

--- assets/page-content-BHPOFcp2.js
+++
@@ -1,1 +0,0 @@
-import{j as e,u as n}from"./main-XReh5g_2.js";const o=({children:t})=>e.jsx(e.Fragment,{children:t});function s(){const{children:t}=n();return e.jsx(o,{children:t})}export{s as default};
---
+++ assets/page-content-CwYzCNR3.js
@@ -0,0 +1,1 @@
+import{j as e,u as n}from"./main-D6d3ys0K.js";const o=({children:t})=>e.jsx(e.Fragment,{children:t});function s(){const{children:t}=n();return e.jsx(o,{children:t})}export{s as default};
