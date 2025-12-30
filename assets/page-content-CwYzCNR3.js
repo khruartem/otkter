aa30f7eaea71deb1a2102544cb1323467f0457f6
Artem Khrustalev <khru-artem@yandex.ru>
2025-12-31 01:56:24 +0300

    Updates

--- assets/page-content-CwYzCNR3.js
+++
@@ -1,1 +0,0 @@
-import{j as e,u as n}from"./main-D6d3ys0K.js";const o=({children:t})=>e.jsx(e.Fragment,{children:t});function s(){const{children:t}=n();return e.jsx(o,{children:t})}export{s as default};
