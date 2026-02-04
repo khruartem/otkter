969be248ba88e8559a942aaf5b2444c0ce4d0190
Artem Khrustalev <khru-artem@yandex.ru>
2026-02-04 10:39:29 +0300

    Updates

---
+++ assets/page-content-BSfmjUwo.js
@@ -0,0 +1,1 @@
+import{j as e,u as n}from"./main-CoR2oN_Z.js";const o=({children:t})=>e.jsx(e.Fragment,{children:t});function s(){const{children:t}=n();return e.jsx(o,{children:t})}export{s as default};
