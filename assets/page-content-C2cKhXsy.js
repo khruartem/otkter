9459c0c88c159cd275e773d3b2913e64bdd3d5e6
Artem Khrustalev <khru-artem@yandex.ru>
2026-02-07 16:43:12 +0300

    Updates

---
+++ assets/page-content-C2cKhXsy.js
@@ -0,0 +1,1 @@
+import{j as e,u as n}from"./main-C-9YaHl6.js";const o=({children:t})=>e.jsx(e.Fragment,{children:t});function s(){const{children:t}=n();return e.jsx(o,{children:t})}export{s as default};
